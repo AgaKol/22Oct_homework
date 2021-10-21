f09eacad068e57805c686c59048269eaba5ce4ff
Aga Koltun <agnieszka.koltun.89@gmail.com>
2021-10-21 16:49:23 +0100

    6 tests total completed

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -8,4 +8,12 @@ def get_total_cash(cc_pet_shop):
 
 
 def add_or_remove_cash(cc_pet_shop, cash):
-    cc_pet_shop["admin"]["total_cash"] = cc_pet_shop["admin"]["total_cash"]+cash
+    cc_pet_shop["admin"]["total_cash"] += cash
+
+
+def get_pets_sold(cc_pet_shop):
+    return cc_pet_shop["admin"]["pets_sold"]
+
+
+def increase_pets_sold(cc_pet_shop, increase):
+    cc_pet_shop["admin"]["pets_sold"] += increase
